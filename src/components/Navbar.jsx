14791d7b2e5b26020a8111d63dde474a05135fdb
Tobi Dele-Safa <delesafatobi@gmail.com>
2021-12-12 21:47:08 +0100

    create and search for recipes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import Searchbar from './Searchbar';
 
 export default function N() {
     return (
         <div className='navbar'>
             <nav>
                 <Link to="/" className='brand'><h1>Cooking Ninja</h1></Link>
+                <Searchbar/>
                 <Link to="/create">Create Recipe</Link>
             </nav>
         </div>
